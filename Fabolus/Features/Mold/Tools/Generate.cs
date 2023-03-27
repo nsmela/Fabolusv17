ec0c59bc5cb3e85812921e3a28eacc423f26bacf
Nathan Smela <nsmela@hotmail.com>
2023-03-27 13:35:35 -0700

    preparing generate mold

---
+++ Fabolus/Features/Mold/Tools/Generate.cs
@@ -0,0 +1,26 @@
+﻿using Fabolus.Features.Bolus;
+using g3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Fabolus.Features.Mold.Tools {
+    public static partial class MoldTools {
+        public static MeshGeometry3D GenerateMold(MoldShape shape) {
+            var mold = BolusUtility.MeshGeometryToDMesh(shape.Geometry as MeshGeometry3D);
+            var bolus = BolusUtility.MeshGeometryToDMesh(shape.Bolus.Geometry as MeshGeometry3D);
+
+            //invert bolus mesh and add mold mesh
+            var editor = new MeshEditor(bolus);
+            editor.ReverseTriangles(bolus.TriangleIndices(), true);
+            editor.AppendMesh(mold);
+
+            //boolean subtract air channels
+            return BolusUtility.DMeshToMeshGeometry(editor.Mesh);
+        }
+    }
+}
