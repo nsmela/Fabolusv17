21e649640a5eb39ddba6e8b66262b630fac51f00
Nathan Smela <nsmela@hotmail.com>
2023-04-06 12:14:49 -0700

    prior to reworking shapes

---
+++ Fabolus/Features/AirChannel/Controls/ChannelControlViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabolus.Features.AirChannel.Controls {
+    public class ChannelControlViewModel : ObservableObject {
+        public virtual string Name { get; }
+        //left mouse label
+        //left mouse control
+
+
+    }
+}
